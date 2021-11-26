42537fb9ec91485b2aefeb9e1df8d14d4ea975e8
Russell Abernethy <55411746+rabernethy@users.noreply.github.com>
2021-11-26 15:58:09 -0500

    version working on windows

--- server.py
+++ server.py
@@ -1,16 +1,30 @@
+# server.py written by Russell Abernethy
+# For Smart Sensors Class @ Temple University
+
+import librosa, torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2CTCTokenizer
 from flask import Flask, request, jsonify, render_template
-#from speech_to_text import speech_to_text
+
+filename = './current_audio.wav'
 
 app = Flask(__name__)
 
 @app.route('/asr',methods=['POST'])
 def asr():
     audio = request.files['file']
-    audio.save('./current_audio.wav')
+    audio.save( filename )
+
+    audio, rate = librosa.load(filename,sr = 16000)
+    input_values = tokenizer(audio, return_tensors = "pt").input_values
+    logits = model(input_values).logits
+    prediction = torch.argmax(logits, dim = -1)
+
+    return jsonify(tokenizer.batch_decode(prediction)[0])
 
 if __name__ == "__main__":
 
-    #model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
-    #tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+    tokenizer = Wav2Vec2CTCTokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+
     app.run(debug=True)
 
