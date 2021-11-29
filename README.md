21d71768dda83dd70c0038f536f8ed2842afe0ea
Russell <rdaswimmer@gmail.com>
2021-11-28 20:21:27 -0500

    added comments and preprocessing

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 CIS 4330 003 Final Course Project
 
+## Pretrained Model
+
 [Pretrained Model wav2vec](https://github.com/pytorch/fairseq/tree/main/examples/wav2vec)
+![Model_Flow_Chart]()
+## Used External Libraries & Tools
+
+* [Librosa](https://librosa.org/doc/latest/index.html)
+* [PyTorch](https://pytorch.org/)
+* [Transformers]()(Specifically Wav2Vec2ForCTC & Wav2Vec2Tokenizer)
+* [Wavfile](https://pypi.org/project/wavefile/)
+* [Flask](https://pypi.org/project/Flask/)
--- server.py
+++ server.py
@@ -1,27 +1,63 @@
 # server.py written by Russell Abernethy
 # For Smart Sensors Class @ Temple University
 
-import librosa, torch
+import librosa, torch, wavfile
+import noisereduce as nr
 from transformers import Wav2Vec2ForCTC, Wav2Vec2CTCTokenizer
 from flask import Flask, request, jsonify, render_template
 
-filename = './current_audio.wav'
+
+
 
 app = Flask(__name__)
 
+
+
 @app.route('/asr',methods=['POST'])
 def asr():
-    audio = request.files['file']
+    filename = './current_audio.wav'
+# SENSOR DATA EXTRACTION
+
+    # Data is sent from an andriod tablet running the app & will collected from microphones on the tablet.
+
+    # Load the audio from the received request and save a temporary copy.
+    audio = request.files['file']  
     audio.save( filename )
 
+# PREPROCESSING
+
+    # Remove background noise that is irrelevant to the meeting that is being transcibed.
+    rate, data = wavfile.read( filename )
+    data = nr.reduce_noise(y=data, sr = 16000)
+        # Noise Reduction Algorithm Workings:
+        # Calculate a spectrogram of the audio clip
+        # Calculate a threshold based on the statistic likelyhood of the noises in the audio file
+        # Calculate a spectrogram over the signal previousely generated
+        # Create a mask that is the difference between the threshold and the spectrogram.
+        # The mask is smoothed using a filter that is applied in the time frequency domain
+        # The mask is applied to the spectrogram of the signal and is inverted.
+    data.save( filename )
+
+# FEATURE EXTRACTION & CLASSIFICATION
+
+    # load the audio file into audio, making sure to sample it at 16000 Hertz
     audio, rate = librosa.load(filename,sr = 16000)
+
+    # tokenize the audio data so that it can be used in the model
     input_values = tokenizer(audio, return_tensors = "pt").input_values
+
+    # store non-normalized predictions based on the logit function applied to the input values
     logits = model(input_values).logits
+
+    # calculate the predicted transcription of the audio file.
     prediction = torch.argmax(logits, dim = -1)
 
+    # return a json object that containing the predicted transcription of the audio
     return jsonify(tokenizer.batch_decode(prediction)[0])
 
 if __name__ == "__main__":
+
+# INITILIZATION OF MODEL & SERVER
 
     model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
     tokenizer = Wav2Vec2CTCTokenizer.from_pretrained("facebook/wav2vec2-base-960h")
