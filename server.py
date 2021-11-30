cfd69587ad343502c34d7019c45cc9abec6c3392
Russell <rdaswimmer@gmail.com>
2021-11-30 08:37:19 -0500

    added wordle to server instead of app

--- server.py
+++ server.py
@@ -3,32 +3,33 @@
 
 import librosa, torch, wavfile
 import noisereduce as nr
+import numpy as npy
+
 from transformers import Wav2Vec2ForCTC, Wav2Vec2CTCTokenizer
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, send_file
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
 
-
-
+result_filename = 'results.txt'
+input_filename = './current_audio.wav'
 
 app = Flask(__name__)
 
-
-
 @app.route('/asr',methods=['POST'])
 def asr():
-    filename = './current_audio.wav'
+    
 # SENSOR DATA EXTRACTION
-
     # Data is sent from an andriod tablet running the app & will collected from microphones on the tablet.
-
     # Load the audio from the received request and save a temporary copy.
     audio = request.files['file']  
-    audio.save( filename )
+    audio.save( input_filename )
 
 # PREPROCESSING
 
     # Remove background noise that is irrelevant to the meeting that is being transcibed.
-    rate, data = wavfile.read( filename )
+    rate, data = wavfile.read( input_filename )
     data = nr.reduce_noise(y=data, sr = 16000)
+    
         # Noise Reduction Algorithm Workings:
         # Calculate a spectrogram of the audio clip
         # Calculate a threshold based on the statistic likelyhood of the noises in the audio file
@@ -36,12 +37,13 @@ def asr():
         # Create a mask that is the difference between the threshold and the spectrogram.
         # The mask is smoothed using a filter that is applied in the time frequency domain
         # The mask is applied to the spectrogram of the signal and is inverted.
-    data.save( filename )
+
+    data.save( input_filename )
 
 # FEATURE EXTRACTION & CLASSIFICATION
 
     # load the audio file into audio, making sure to sample it at 16000 Hertz
-    audio, rate = librosa.load(filename,sr = 16000)
+    audio, rate = librosa.load(input_filename,sr = 16000)
 
     # tokenize the audio data so that it can be used in the model
     input_values = tokenizer(audio, return_tensors = "pt").input_values
@@ -52,8 +54,22 @@ def asr():
     # calculate the predicted transcription of the audio file.
     prediction = torch.argmax(logits, dim = -1)
 
-    # return a json object that containing the predicted transcription of the audio
-    return jsonify(tokenizer.batch_decode(prediction)[0])
+#POST PROCESSING
+
+    #use the predicted transcription of the audio to generate a wordle
+    results = tokenizer.batch_decode(prediction)[0]
+
+    # save the results to a textfile
+    with open(result_filename,"w") as f:
+        for w in results:
+            f.write(w + "/n")
+
+    # create a word visuluzation using to results from the ASR
+    data = open("results.txt").read()
+    data = data.lower()
+    create_word_cloud("picture_result.png")
+
+    return send_file("results.txt", mimetype='image/png')
 
 if __name__ == "__main__":
 
@@ -64,3 +80,11 @@ if __name__ == "__main__":
 
     app.run(debug=True)
 
+
+# wordle helper function
+def create_word_cloud(string):
+    
+   maskarray = npy.array(Image.open("cloud.png"))
+   cloud = WordCloud(background_color = "white", max_words = 200, mask = maskarray, stopwords = set(STOPWORDS))
+   cloud.generate(string)
+   cloud.to_file("wordCloud.png")
